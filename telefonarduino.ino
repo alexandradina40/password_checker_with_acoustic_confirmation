279c2b6002b697c0d2e1655a75047604b9f8a698
Dina <alexandra.dina40@yahoo.com>
2023-02-16 18:03:46 +0200

    initial commit

---
+++ telefonarduino.ino
@@ -0,0 +1,404 @@
+#include <string.h>
+int pin0=0,pin1=0,pin2=0,pin3=0,pin4=0,pin5=0,pin6=0,pin7=0;
+int parola[4];
+int x=0;
+String p;
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define REST      0
+
+int tempo = 114;
+
+// change this to whichever pin you want to use
+
+
+// notes of the moledy followed by the duration.
+// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+// !!negative numbers are used to represent dotted notes,
+// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
+int melody[] = {
+
+  // Take on me, by A-ha
+  // Score available at https://musescore.com/user/27103612/scores/4834399
+  // Arranged by Edward Truong
+
+   NOTE_D5,-4, NOTE_E5,-4, NOTE_A4,4, //1
+  NOTE_E5,-4, NOTE_FS5,-4, NOTE_A5,16, NOTE_G5,16, NOTE_FS5,8,
+  NOTE_D5,-4, NOTE_E5,-4, NOTE_A4,2,
+  NOTE_A4,16, NOTE_A4,16, NOTE_B4,16, NOTE_D5,8, NOTE_D5,16,
+  NOTE_D5,-4, NOTE_E5,-4, NOTE_A4,4, //repeat from 1
+  NOTE_E5,-4, NOTE_FS5,-4, NOTE_A5,16, NOTE_G5,16, NOTE_FS5,8,
+  NOTE_D5,-4, NOTE_E5,-4, NOTE_A4,2,
+  NOTE_A4,16, NOTE_A4,16, NOTE_B4,16, NOTE_D5,8, NOTE_D5,16,
+  REST,4, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_D5,8, NOTE_E5,8, NOTE_CS5,-8,
+  NOTE_B4,16, NOTE_A4,2, REST,4, 
+
+  REST,8, NOTE_B4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,4, NOTE_A4,8, //7
+  NOTE_A5,8, REST,8, NOTE_A5,8, NOTE_E5,-4, REST,4, 
+  NOTE_B4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8, NOTE_D5,8, NOTE_E5,8, REST,8,
+  REST,8, NOTE_CS5,8, NOTE_B4,8, NOTE_A4,-4, REST,4,
+  REST,8, NOTE_B4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8, NOTE_A4,4,
+  NOTE_E5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,4, REST,4,
+   
+  NOTE_D5,2, NOTE_E5,8, NOTE_FS5,8, NOTE_D5,8, //13
+  NOTE_E5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,4, NOTE_A4,4,
+  REST,2, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8,
+  REST,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+
+  NOTE_E5,-8, NOTE_E5,-8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,-8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16, //18
+  NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,8, NOTE_A4,8, NOTE_A4,8, 
+  NOTE_E5,4, NOTE_D5,2, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_A5,4, NOTE_CS5,8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+
+  NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,4, NOTE_A4,8,  //23
+  NOTE_E5,4, NOTE_D5,2, REST,4,
+  REST,8, NOTE_B4,8, NOTE_D5,8, NOTE_B4,8, NOTE_D5,8, NOTE_E5,4, REST,8,
+  REST,8, NOTE_CS5,8, NOTE_B4,8, NOTE_A4,-4, REST,4,
+  REST,8, NOTE_B4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8, NOTE_A4,4,
+  REST,8, NOTE_A5,8, NOTE_A5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8, NOTE_D5,8,
+  
+  REST,8, NOTE_A4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8, //29
+  REST,8, NOTE_CS5,8, NOTE_B4,8, NOTE_A4,-4, REST,4,
+  NOTE_B4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8, NOTE_A4,4, REST,8,
+  REST,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,4, NOTE_E5,-4, 
+  NOTE_D5,2, NOTE_D5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,4, 
+  NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8, NOTE_A4,8, NOTE_A4,4,
+
+  REST,-4, NOTE_A4,8, NOTE_B4,8, NOTE_CS5,8, NOTE_D5,8, NOTE_B4,8, //35
+  REST,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_E5,-8, NOTE_E5,-8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,4, NOTE_A4,8, 
+
+   NOTE_E5,4, NOTE_D5,2, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16, //40
+  NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_A5,4, NOTE_CS5,8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,4, NOTE_A4,8,  
+  NOTE_E5,4, NOTE_D5,2, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+   
+  NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16, //45
+  NOTE_A5,4, NOTE_CS5,8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,4, NOTE_A4,8,  
+  NOTE_E5,4, NOTE_D5,2, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_FS5,-8, NOTE_FS5,-8, NOTE_E5,-4, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16, //45
+  
+  NOTE_A5,4, NOTE_CS5,8, NOTE_D5,-8, NOTE_CS5,16, NOTE_B4,8, NOTE_A4,16, NOTE_B4,16, NOTE_D5,16, NOTE_B4,16,
+  NOTE_D5,4, NOTE_E5,8, NOTE_CS5,-8, NOTE_B4,16, NOTE_A4,4, NOTE_A4,8, 
+
+  NOTE_E5,4, NOTE_D5,2, REST,4
+};
+
+// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+// there are two values per note (pitch and duration), so for each note there are four bytes
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+
+// this calculates the duration of a whole note in ms
+int wholenote = (60000 * 4) / tempo;
+
+int divider = 0, noteDuration = 0;
+
+void setup() {
+   Serial.begin(9600);
+   pinMode(2, INPUT);
+   pinMode(3, INPUT);
+   pinMode(4, INPUT);
+   pinMode(5, INPUT);
+   pinMode(6, OUTPUT);
+   pinMode(7, OUTPUT);
+   pinMode(8, OUTPUT);
+   pinMode(9, OUTPUT);
+   pinMode(10,OUTPUT);
+   digitalWrite(6,LOW);
+   digitalWrite(7,LOW);
+   digitalWrite(8,LOW);
+   digitalWrite(9,LOW);
+    parola[0]=0;
+    parola[1]=0;
+    parola[2]=0;
+    parola[3]=0;
+
+    
+}
+
+void loop() {
+    delay(1000);
+
+  
+    digitalWrite(6,HIGH);
+    
+     pin0= digitalRead(2);
+     pin1= digitalRead(3);
+     pin2= digitalRead(4);
+     pin3= digitalRead(5);
+
+    if(pin0==HIGH){
+      Serial.print(1); 
+      Serial.print("\n");
+      parola[x]=1;
+      x++;
+    }
+   
+
+
+    if(pin1==HIGH){
+      Serial.print(2);
+      Serial.print("\n");
+      parola[x]=2;
+      x++;
+    }
+
+    if(pin2==HIGH){
+      Serial.print(3);
+      Serial.print("\n");
+      parola[x]=3;
+      x++;
+    }
+
+    if(pin3==HIGH){
+      Serial.print('A'); 
+      Serial.print("\n");
+      parola[x]=10;
+      x++;
+    }
+    
+    digitalWrite(6,LOW);
+    digitalWrite(7,HIGH);
+
+
+    pin0= digitalRead(2);
+     pin1= digitalRead(3);
+     pin2= digitalRead(4);
+     pin3= digitalRead(5);
+    
+      if(pin0==HIGH){
+      Serial.print(4); 
+      Serial.print("\n");
+      parola[x]=4;
+      x++;
+    }
+
+    if(pin1==HIGH){
+      Serial.print(5);
+      Serial.print("\n");
+      parola[x]=5;
+      x++;
+    }
+
+    if(pin2==HIGH){
+      Serial.print(6);
+      Serial.print("\n");
+      parola[x]=6;
+      x++;
+    }
+
+    if(pin3==HIGH){
+      Serial.print('B'); 
+      Serial.print("\n");
+      parola[x]=11;
+      x++;
+    }
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,HIGH);
+
+
+    pin0= digitalRead(2);
+     pin1= digitalRead(3);
+     pin2= digitalRead(4);
+     pin3= digitalRead(5);
+    
+      if(pin0==HIGH){
+      Serial.print(7); 
+      Serial.print("\n");
+      parola[x]=7;
+      x++;
+    }
+
+    if(pin1==HIGH){
+      Serial.print(8);
+      Serial.print("\n");
+      parola[x]=8;
+      x++;
+    }
+
+    if(pin2==HIGH){
+      Serial.print(9);
+      Serial.print("\n");
+      parola[x]=9;
+      x++;
+    }
+
+    if(pin3==HIGH){
+      Serial.print('C'); 
+      Serial.print("\n");
+      parola[x]=12;
+      x++;
+    }
+
+    digitalWrite(8,LOW);
+    digitalWrite(9,HIGH);
+
+
+    pin0= digitalRead(2);
+     pin1= digitalRead(3);
+     pin2= digitalRead(4);
+     pin3= digitalRead(5);
+    
+      if(pin0==HIGH){
+      Serial.print('*'); 
+      Serial.print("\n");
+      parola[x]=15;
+      x++;
+    }
+
+    if(pin1==HIGH){
+      Serial.print("0");
+      Serial.print("\n");
+      parola[x]=0;
+      x++;
+    }
+
+    if(pin2==HIGH){
+      Serial.print('#');
+      Serial.print("\n");
+      parola[x]=14;
+      x++;
+    }
+
+    if(pin3==HIGH){
+      Serial.print('D'); 
+      Serial.print("\n");
+      parola[x]=13;
+      x++;
+    }
+    digitalWrite(9,LOW);
+    
+    for(int i=0;i<4;i++)
+    {
+      Serial.print(parola[i]);
+    }
+    Serial.print("\n");
+    if(parola[0]==1 && parola[1]==3 && parola[2]==3 && parola[3]==7)
+    {
+       for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) 
+     {
+        
+
+           // calculates the duration of each note
+        divider = melody[thisNote + 1];
+        if (divider > 0) 
+        {
+          
+          // regular note, just proceed
+          noteDuration = (wholenote) / divider;
+        
+        } else 
+        if (divider < 0) 
+        {
+          // dotted notes are represented with negative durations!!
+          noteDuration = (wholenote) / abs(divider);
+          noteDuration *= 1.5; // increases the duration in half for dotted notes
+        }
+
+          // we only play the note for 90% of the duration, leaving 10% as a pause
+          tone(10, melody[thisNote], noteDuration * 0.9);
+      
+          // Wait for the specief duration before playing the next note.
+          delay(noteDuration);
+      
+          // stop the waveform generation before the next note.
+          noTone(10);
+    }
+  }
+    }
+   // Serial.print(p);
+  
